69573db37879d8144ac01b2891e79d2ef9cb5ee1
Rober <rp_rober@hotmail.com>
2023-04-21 17:32:07 +0200

    refactor

--- src/components/Ads/AdDetail.js
+++ src/components/Ads/AdDetail.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getAd } from "./service";
 import Layout from "../layout/Layout";
 import "./AdsPage.css";
@@ -8,15 +8,24 @@ import DefaultPhoto from "../shared/DefaultPhoto";
 
 const AdDetail = props => {
   const params = useParams();
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
   const [ad, setAd] = useState(null);
 
   useEffect(() => {
-    getAd(params.id).then(ad => setAd(ad));
-  }, [params.id]);
+    getAd(params.id)
+      .then(ad => setAd(ad))
+      .catch(error => {
+        if (error.status === 404) {
+          return navigate("/404");
+        }
+        setError(error);
+      });
+  }, [params.id, navigate]);
 
   return (
     <Layout title="Detalle del anuncio" {...props}>
-      {ad && (
+      {(ad || error) && (
         <div className="ad-detail">
           <div className="ad">
             <span className="text"> articulo {ad.name}</span>
